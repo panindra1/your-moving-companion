8f4828bb2ef6a0f62eb0d78b96cd1a48740c765c
Panindra <pani.1990@gmail.com>
2015-01-17 17:18:40 -0600

    final version

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var street = $('#street').val();
+    var location = $('#city').val();
+    var address = street + "," + location;
+
+    if(street != '' && location != '') {
+        var url = "https://maps.googleapis.com/maps/api/streetview?size= 600x300&location="+address; 
+        //alert(url );
+        $body.append('<img class = "bgimg" src="'+url+'"></img>');
+    }
+
+    var nyUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+location+"&api-key=cb97ee31cf2dbcb680695c8dd2d1ba75:8:70882968";
+    
+    $.getJSON( nyUrl, function( data ) {
+        $nytHeaderElem.text("New York Times Articles about" + location);
+        $.each( data.response.docs, function( key, val ) {
+            var article = data.response.docs[key];
+            $nytElem.append( "<li calss ='article'> <a href ='"+ article.web_url+"'>"+article.headline.main+"</a><p>"+article.snippet+"</p></li>" );
+        });
+    })
+    .error(function() {
+        $nytHeaderElem.text("New York Times Articles about" + location+ "cannot be loaded");
+    });
+    
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+location+"&format=json&callback=wikicallback";
+    //alert(wikiUrl);
+    $.ajax( {
+        url: wikiUrl,
+        dataType:'jsonp',
+        success: function(data) {
+            var articleList = data[1];
+            console.log(articleList);
+            
+            $.each( articleList, function( key, val ) {
+             var articleStr = articleList[key];
+             var url = "http://en.wikipedia.org/wiki/" + articleStr;
+            $wikiElem.append( "<li> <a href ='"+ url+"'>"+articleStr+"</a></li>" );
+        });      
+        }
+    });
+
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
+
+// loadData();
